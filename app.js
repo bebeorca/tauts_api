d7941e6b4e610b26afd4a4a1092d75f5bcfb74e1
bebeorca <dimasrdyo@gmail.com>
2025-05-22 18:17:31 +0700

    cors update

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
 const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 const authRoutes = require("./src/routes/auth.route");
 const linkRoutes = require("./src/routes/link.route");
 const sequelize = require("./src/config/db");
 
-app.use(cors({
-  origin: 'http://localhost:5173',
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://tauts.vercel.app"],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/link", linkRoutes);
-app.get('/api', (req, res) => {
-  res.status(200).json({ status: 'ok' });
+app.get("/api", (req, res) => {
+  res.status(200).json({ status: "ok" });
 });
 
 sequelize.sync().then(() => {
